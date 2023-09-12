5b3c21c195f42449fc98be6766fd4e408b00add5
amj132 <amanjazizi@yahoo.com>
2023-09-13 00:12:35 +0330

    Begining of Project Detail Design 001

--- Projects/urls.py
+++ Projects/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="projects_index" ),
+    path("<int:project_id>", views.projectdetail, name="projects_projectdetail")
 ]
--- Projects/views.py
+++ Projects/views.py
@@ -1,9 +1,16 @@
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProjectList
 
 # Create your views here.
 def index(request):
     projects = ProjectList.objects.all()
     return render(request, "projects\projects.html", {"projects": projects})
-    #return HttpResponse("Apps Started to be Creating")
+    #strprojects = ", ".join([p.project for p in projects])
+    #return HttpResponse(strprojects)
+
+def projectdetail(request, project_id):
+    projdetail = get_object_or_404(ProjectList, id=project_id)
+    context = {"projdetail": projdetail}
+    return render(request, "projects\projectdetail.html", context)
+
